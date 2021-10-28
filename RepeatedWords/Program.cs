cde038da5de28a9e97a12ea3b7383bee23bf7d4e
wiktomi <youmajktube@gmail.com>
2021-10-28 18:18:31 +0200

    Add project files.

---
+++ RepeatedWords/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace RepeatedWords
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var filePath = "C:\\Users\\TURBO\\source\\repos\\RepeatedWords\\RepeatedWords\\test.txt";
+
+            string allWords = File.ReadAllText(filePath, Encoding.UTF8).ToLower();
+
+            string[] wordsArray = allWords.Split(' ',',', '.', '-', '!');
+
+            List<WordCounter> wordCounters = new List<WordCounter>();
+
+            foreach (string w in wordsArray)
+            {
+               WordCounter foundWord =  wordCounters.Find(x => x.Word == w);
+
+                if(foundWord == null)
+                {
+                    wordCounters.Add(new WordCounter(w, 1));
+                }
+                else
+                {
+                    foundWord.Frequency++;
+                }
+            }
+
+            foreach (var item in wordCounters)
+            {
+                Console.WriteLine($"Word: {item.Word} Frequency: {item.Frequency}");
+            }
+
+        }
+    }
+    class WordCounter
+    {
+        public WordCounter(string word, int frequency)
+        {
+            Word = word;
+            Frequency = frequency;
+        }
+
+        public string Word { get; set; }
+        public int Frequency { get; set; }
+
+    }
+}
